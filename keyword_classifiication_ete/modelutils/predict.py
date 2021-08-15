6226ab032511843af9168efa832bd76338b465d1
mosrihari <mosrihari@gmail.com>
2021-08-15 21:29:09 +0800

    minor changes

--- keyword_classifiication_ete/modelutils/predict.py
+++ keyword_classifiication_ete/modelutils/predict.py
@@ -1,8 +1,22 @@
 import settings
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import re
+import spacy
+spacy_nlp = spacy.load('en_core_web_sm')
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(settings.REPLACE_BY_SPACE_RE, "", string=text)
+    text = re.sub(settings.BAD_SYMBOLS_RE, "", string=text)
+    text = text.replace("x", "")
+    doc = spacy_nlp(text)
+    lemmatized_no_stopwords = ' '.join([token.lemma_ for token in doc if not token.is_stop])
+    return lemmatized_no_stopwords
 
 def predict_text(new_complaint, model, tokenizer):
+    new_complaint = clean_text(new_complaint[0])
+    new_complaint = [new_complaint]
     seq = tokenizer.texts_to_sequences(new_complaint)
     padded = pad_sequences(seq, maxlen=settings.MAX_SEQUENCE_LENGTH)
     pred = model.predict(padded)
--- keyword_classifiication_ete/settings.py
+++ keyword_classifiication_ete/settings.py
@@ -1,8 +1,13 @@
+import re
+
 MAX_SEQUENCE_LENGTH = 250
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
 labels = ['Bank account or service', 'Consumer Loan', 'Credit card', \
-       'Credit reporting', 'Debt collection', 'Money transfers', 'Mortgage', \
+       'Credit reporting', 'Debt collection',
+          'Money transfers', 'Mortgage', \
        'Other financial service', 'Payday loan', 'Prepaid card', \
        'Student loan']
 
-model_file_path = r"C:\Users\srihari_mohan\Documents\PersonalProjects\KeywordClassification\keyword_model.h5"
-tokenizer_path = r"C:\Users\srihari_mohan\Documents\PersonalProjects\KeywordClassification\tokenizer.pkl"
+model_file_path = r"C:\Users\srihari_mohan\Documents\PersonalProjects\keywordclassify\KeywordClassification\keyword_model.h5"
+tokenizer_path = r"C:\Users\srihari_mohan\Documents\PersonalProjects\keywordclassify\KeywordClassification\tokenizer.pkl"
